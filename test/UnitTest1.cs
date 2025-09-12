c5089845efc3ddbb7038044ac4f690653d9f8a59
AnsiDK <andreas.dunk@gmail.com>
2025-09-12 10:28:28 +0200

    Added simple tests

---
+++ test/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+
+public class ProgramTests
+{
+    [Fact]
+    public void Main_WithCheepArgument_DoesNotThrow()
+    {
+        // Arrange
+        string[] args = new[] { "cheep", "Hello, test!" };
+
+        // Act & Assert
+        var exception = Record.Exception(() => Program.Main(args));
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void Main_WithNoArguments_DoesNotThrow()
+    {
+        // Arrange
+        string[] args = Array.Empty<string>();
+
+        // Act & Assert
+        var exception = Record.Exception(() => Program.Main(args));
+        Assert.Null(exception);
+    }
+}
