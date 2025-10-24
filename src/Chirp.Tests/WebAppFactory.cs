3e6325464f68b2f1a0af84c4fcaa12eead9fb8eb
Kolomito <luvp@itu.dk>
2025-10-24 15:25:28 +0200

    Trying to fix tests

--- src/Chirp.Tests/WebAppFactory.cs
+++ src/Chirp.Tests/WebAppFactory.cs
@@ -4,11 +4,15 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System;
 
 namespace Chirp.Tests;
 
-public class WebAppFactory : WebApplicationFactory<Chirp.Web.Program>
+public class WebAppFactory : WebApplicationFactory<Chirp.Web.Program>, IDisposable
 {
+    private SqliteConnection? _connection;
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureServices(services =>
@@ -19,11 +23,11 @@ public class WebAppFactory : WebApplicationFactory<Chirp.Web.Program>
             if (descriptor is not null)
                 services.Remove(descriptor);
 
-            // Use in-memory SQLite for tests
-            var connection = new SqliteConnection("DataSource=:memory:");
-            connection.Open();
+            // create a single persistent in-memory SQLite connection
+            _connection = new SqliteConnection("Data Source=:memory:;");
+            _connection.Open();
 
-            services.AddDbContext<ChirpDbContext>(o => o.UseSqlite(connection));
+            services.AddDbContext<ChirpDbContext>(o => o.UseSqlite(_connection));
 
             // Ensure database and seed
             var sp = services.BuildServiceProvider();
@@ -32,5 +36,20 @@ public class WebAppFactory : WebApplicationFactory<Chirp.Web.Program>
             db.Database.EnsureCreated();
             DbInitializer.SeedDatabase(db);
         });
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        if (disposing)
+        {
+            try
+            {
+                _connection?.Close();
+                _connection?.Dispose();
+                _connection = null;
+            }
+            catch (Exception) { /* swallow on dispose */ }
+        }
     }
 }
